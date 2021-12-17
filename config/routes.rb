2a801e17a42505fb7298146bdd34497e1f47c942
septem <qitengyuxi@gmail.com>
2021-12-17 17:17:01 +0900

    チャットルームroutes/controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ root 'top#index'
 resources :users, only: [:show, :index]
 resources :reactions, only: [:create]
 resources :matching, only: [:index]
+resources :chat_rooms, only: [:create, :show]
 end
